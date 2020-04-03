942c1230251106d170bb1838095d72227de21c3f
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-04-03 16:03:19 -0700

    first commit

---
+++ src/components/__tests__/CommentBox.Test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+it('has a text area and a button', () => {
+  const wrapped = mount(<CommentBox />);
+});
+
+console.log(wrapped.find('textarea').length);
