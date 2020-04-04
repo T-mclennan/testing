da18fb2ffa85dff4e40da1edbca9f29afa003bfb
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-04-03 17:11:15 -0700

    exploring ways to assert checks on props and internal values

--- src/components/__tests__/CommentBox.Test.js
+++ src/components/__tests__/CommentBox.Test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
-it('has a text area and a button', () => {
-  const wrapped = mount(<CommentBox />);
+let wrapped;
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
 });
 
-console.log(wrapped.find('textarea').length);
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a text area that users can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' },
+  });
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
