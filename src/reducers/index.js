f5cbaaf387d804302f55039d101b90b40215cece
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-04-08 16:42:43 -0700

    added basic auth route, setting up tests for higher order components

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import authReducer from 'reducers/auth';
 import commentReducer from './comments';
 
 export default combineReducers({
+  auth: authReducer,
   comments: commentReducer,
 });
